d2adce039983d051fee469df0ea20717a7c90b3f
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2021-01-18 10:55:29 +0800

    Updated project name

--- wp-gatsby-testsite/gatsby-config.js
+++ wp-gatsby-testsite/gatsby-config.js
@@ -1,12 +1,12 @@
 require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+  path: `.env.${process.env.NODE_ENV}`
 })
 
 module.exports = {
   siteMetadata: {
     title: `Wordpress as a headless CMS with Gatsby`,
     description: `Simple starter for a Gatsby project fetching data from Wordpress via the gatsby-source-wordpress@v4 plugin`,
-    author: `kurt@lekanger.no`,
+    author: `kurt@lekanger.no`
   },
   plugins: [
     `gatsby-plugin-postcss`,
@@ -15,8 +15,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -29,22 +29,22 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
     },
     {
       resolve: `gatsby-source-wordpress-experimental`,
       options: {
-        url: process.env.GATSBY_GRAPHQL_URL,
-      },
+        url: process.env.GATSBY_GRAPHQL_URL
+      }
     },
     {
       resolve: "gatsby-source-graphql",
       options: {
         typeName: "WPGraphQL",
         fieldName: "wpgraphql",
-        url: process.env.GATSBY_GRAPHQL_URL,
-      },
-    },
-  ],
+        url: process.env.GATSBY_GRAPHQL_URL
+      }
+    }
+  ]
 }
